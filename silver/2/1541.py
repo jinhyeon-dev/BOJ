d298b732bc8d9c521b408689ebb3b3eba9f729f5
jinhyeon-dev <jinhyeon.dev@gmail.com>
2024-09-24 23:14:48 +0900

    algorithm

---
+++ silver/2/1541.py
@@ -0,0 +1,17 @@
+par = input().split('-')
+
+num = []
+
+for i in par:
+    sum = 0
+    tmp = i.split('+') 
+    for j in tmp: 
+        sum += int(j)
+    num.append(sum) 
+
+n = num[0]
+
+for i in range(1,len(num)):
+    n -= num[i]
+    
+print(n)

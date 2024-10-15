61ccfd1e2e3f06bdd1e8b03bb2897db5eae5bc7e
jinhyeon-dev <jinhyeon.dev@gmail.com>
2024-10-15 12:19:16 +0900

    10/15

---
+++ silver/5/10814.py
@@ -0,0 +1,8 @@
+a = int(input())
+user = []
+for _ in range(a):
+    age, name = input().split()
+    user.append([int(age),name])
+
+for i in sorted(user, key=lambda x : x [0]):
+    print(i[0],i[1])
